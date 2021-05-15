c4a06879063b4a4c504828dbf1ecdd6778dfa54d
Denis Andreyanov <andreyanov.denis92@gmail.com>
2021-05-15 14:59:08 +0300

    revert TestDataConfig

--- src/test/java/cloud/autotests/config/demowebshop/TestDataConfig.java
+++ src/test/java/cloud/autotests/config/demowebshop/TestDataConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/testdata/testdata.properties"
+        "classpath:config/demowebshop/testdata.properties"
 })
 public interface TestDataConfig extends Config {
     @Key("web.url")

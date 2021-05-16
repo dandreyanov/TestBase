4e1f6bd82765e95cce0d275bef10a5a6355be348
Denis Andreyanov <andreyanov.denis92@gmail.com>
2021-05-16 12:46:28 +0300

    fix create json method

--- src/test/java/testbase/helpers/JsonHelper.java
+++ src/test/java/testbase/helpers/JsonHelper.java
@@ -1,5 +1,6 @@
 package testbase.helpers;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import java.io.IOException;
@@ -7,7 +8,6 @@ import java.io.IOException;
 public class JsonHelper {
     public static JSONObject createJSONdoRegister(String email, String name, String password) {
         JSONObject resultJson = new JSONObject();
-
         resultJson.put("email", email);
         resultJson.put("name", name);
         resultJson.put("password", password);
@@ -15,40 +15,18 @@ public class JsonHelper {
         return (resultJson);
     }
 
-    public static String createJSONcreateCompany(String company_name, String company_type, String firstCompanyUsers, String secondCompanyUsers, String emailOwner) throws IOException {
-//        resultJson.put("company_name",company_name);
-//        resultJson.put("company_type",company_type);
-//        resultJson.put("email_owner", emailOwner);
-/*
-{
-  "company_name": "Алкоголики и тунеядцы",
-  "company_type": "ООО",
-  "company_users": ["test_anna@gmail.com", "mrak20@list.ru"],
-  "email_owner": "aa+1@mail.com"
-}
+    public static JSONObject createJSONcreateCompany(String company_name, String company_type, String firstCompanyUsers, String secondCompanyUsers, String emailOwner) throws IOException {
+        JSONObject resultJson = new JSONObject();
+        JSONArray companiesArr = new JSONArray();
+        companiesArr.add(firstCompanyUsers);
+        companiesArr.add(secondCompanyUsers);
+        resultJson.put("company_name",company_name);
+        resultJson.put("company_type",company_type);
+        resultJson.put("email_owner", emailOwner);
+        resultJson.put("company_users", companiesArr);
 
- */
-        String resultJson = "{\n" +
-                "  \"company_name\": \"" + company_name + "\",\n" +
-                "  \"company_type\": \"" + company_type + "\",\n" +
-                "  \"company_users\": [\"" + firstCompanyUsers + "\", \"" + secondCompanyUsers + "\"],\n" +
-                "  \"email_owner\": \"" + emailOwner + "\"\n" +
-                "}";
-
-        System.out.println(resultJson);
         return (resultJson);
     }
-
-//    public static JSONObject createJSON (String...vars) {
-//        JSONObject resultJson = new JSONObject();
-//
-//        for (String var: vars) {
-//            resultJson.put(var, var);
-//        }
-//
-//        System.out.println(resultJson);
-//        return (resultJson);
-
 
 }
 
